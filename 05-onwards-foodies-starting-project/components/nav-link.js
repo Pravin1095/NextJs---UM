ede2c7994012eac251f18eabbdd486d7c4cdad78
Pravin A <a.pravin3210@gmail.com>
2026-07-04 23:42:39 +0530

    Using Client Components efficiently by creating separate Client Component for NavLinks

---
+++ 05-onwards-foodies-starting-project/components/nav-link.js
@@ -0,0 +1,11 @@
+'use client'
+
+import Link from "next/link";
+import classes from './nav-link.module.css'
+import { usePathname } from "next/navigation";
+export default function NavLink({href, children}){
+    const path = usePathname();
+    return(
+         <Link className={path.startsWith(href) ? classes.active : undefined}  href={href}>{children}</Link> 
+    )
+}
