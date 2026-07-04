dcdfd46aad463e13ea071307defdf03e7e40435e
Pravin A <a.pravin3210@gmail.com>
2026-07-04 19:21:31 +0530

    99. Styling using cssModules

--- 05-onwards-foodies-starting-project/components/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png"
+import classes from './main-header.module.css'
 export default function MainHeader(){
     return(
-        <header>
-            <Link href="/"><img src={logoImg.src} alt="A plate with food on it" />NextLevel Food</Link>
-            <nav>
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/"><img src={logoImg.src} alt="A plate with food on it" />NextLevel Food</Link>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
